5c5ec3ec3817984cad76a5565bfbf65800c39589
Alexander <MrRandomgamerdkHD@gmail.com>
2015-10-28 16:39:29 +0100

    Added VERY basic kernel

---
+++ kernel/kernel.py
@@ -0,0 +1,12 @@
+
+def bootLoader():
+    print("Welcome to SquirrelOS!")
+    print("Made By Dem_Squirrel and FreakzDK \n")
+    print("Type help to see the commands")
+    commandLine()
+
+def commandLine():
+    cmd = input("> ")
+
+
+bootLoader()
