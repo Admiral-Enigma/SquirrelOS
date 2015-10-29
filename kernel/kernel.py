d132525ed2392017fc41e9d369df70726cf95805
DemSquirrel <MrRandomgamerdkHD@gmail.com>
2015-10-29 12:10:55 +0100

    Added some basic commands to the kernel

--- kernel/kernel.py
+++ kernel/kernel.py
@@ -1,3 +1,5 @@
+import sys, os
+commandList = ["help", "shutdown", "ls", "dir"]
 
 def bootLoader():
     print("Welcome to SquirrelOS!")
@@ -7,6 +9,28 @@ def bootLoader():
 
 def commandLine():
     cmd = input("> ")
+    if cmd == commandList[0]:
+        helpList()
+    if cmd == commandList[1]:
+        shutDown()
+    if cmd == commandList[2] or cmd == commandList[3]:
+        showDir()
 
+    for command in commandList:
+        if cmd != commandList:
+            print("This command wasn't found")
+            commandLine()
+        else:
+            break
+
+def helpList():
+    for cmd in commandList:
+        print(cmd)
+    commandLine()
+def shutDown():
+    sys.exit(0)
+
+def showDir():
+    print(os.getcwd())
 
 bootLoader()
